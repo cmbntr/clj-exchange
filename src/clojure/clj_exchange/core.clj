f09adb00a676f615e55060d69901111b47f8ad6b
Michael Locher <cmbntr@gmail.com>
2012-01-05 02:03:27 +0100

    resolve-names operation

--- src/clojure/clj_exchange/core.clj
+++ src/clojure/clj_exchange/core.clj
@@ -1,10 +1,34 @@
 (ns clj-exchange.core
   (:gen-class)
   (:use [clj-exchange ews mapping time])
-  (:import [com.microsoft.schemas.exchange.services._2006.messages FindFolderType FindItemType]))
+  
+  (:import [com.microsoft.schemas.exchange.services._2006.messages
+            FindFolderType
+            FindItemType
+            ResolveNamesType]))
 
 ;;Service Description:
 ;;http://msdn.microsoft.com/en-us/library/bb409286(v=exchg.140).aspx
+
+(defn- parse-response-flat [r]
+  (into {} (mapcat #(-> % :JAXBElement :value)
+                   (-> r
+                       parse-response
+                       :ResponseMessages
+                       :createItemResponseMessageOrDeleteItemResponseMessageOrGetItemResponseMessage))))
+
+(defn resolve-names [svc n]
+  (let [r (-> (build ResolveNamesType {:UnresolvedEntry n :ReturnFullContactData true})
+              (ex-resolve-names svc)
+              parse-response-flat)]
+    (condp =  (:ResponseCode r)
+      "NoError" (-> r :ResolutionSet :Resolution first)
+      "ErrorNameResolutionNoResults" nil
+      
+      (if-let [possibilities (-> r :ResolutionSet :Resolution)]
+        (if (= 1 (count possibilities))
+          (first possibilities)
+          possibilities)))))
 
 (defn find-folder [svc]
   (-> (build FindFolderType
