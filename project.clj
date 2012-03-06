7478c014381633e4d1ff240e57022dd34be02e15
Michael Locher <cmbntr@gmail.com>
2012-03-07 00:12:22 +0100

    bump versions and make it run with leiningen 1.7.0

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
 (defproject clj-exchange "1.0.0-SNAPSHOT"
   :description "Utility for Exchange Web Service Access"
+  :url "https://github.com/cmbntr/clj-exchange"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
-                 [org.codehaus.jackson/jackson-xc "1.9.3"]
-                 [joda-time/joda-time "2.0"]]
+                 [org.clojure/data.json "0.1.2"]
+                 [org.codehaus.jackson/jackson-xc "1.9.5"]
+                 [joda-time/joda-time "2.1"]]
 
   :dev-dependencies [[midje "1.3.1"]
-                     [lein-marginalia "0.6.1"]]
-  
+                     [lein-marginalia "0.7.0"]]
+
   :source-path "src/clojure"
   :resources-path "src/resource"
-  :java-source-path [["src/java"]]
-  
-  :aot [clj-exchange.core clj-exchange.ews clj-exchange.prefs]
-  :main clj-exchange.core)
+  :java-source-path "src/java"
+
+  :aot [clj-exchange.core clj-exchange.main clj-exchange.ews clj-exchange.prefs]
+  :main clj-exchange.main)
 
